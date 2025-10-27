4ddfd16addc46f5347483fec91bc32aff82bde19
Светлана Ситникова <sitnikovasveta885@gmail.com>
2025-10-27 22:56:00 +1000

    geb

--- Tarotora/Prosmotrkolod.xaml.cs
+++ Tarotora/Prosmotrkolod.xaml.cs
@@ -6,7 +6,8 @@ namespace Tarotora;
 public partial class Prosmotrkolod : ContentPage
 {
     DBfuncional db;
-	public Prosmotrkolod(DBfuncional database)
+    private Card currentCard;
+    public Prosmotrkolod(DBfuncional database)
 	{
 		InitializeComponent();
         BindingContext = this;
@@ -17,6 +18,7 @@ public partial class Prosmotrkolod : ContentPage
     {
         var cards = await db.GetCard();
         Cards = cards;
+        OnPropertyChanged(nameof(Cards));
     }
    
     public List<Card> Cards { get; set; }
@@ -32,6 +34,43 @@ public partial class Prosmotrkolod : ContentPage
 
     private async void CardEdit(object sender, EventArgs e)
     {
-            await Navigation.PushAsync(new Addcards(db));
+        var button = (Button)sender;
+        var card = button.CommandParameter as Card;
+
+        //// Открываем страницу редактирования, передаём карту и базу
+        //var editPage = new Addcards(db, card);
+
+        //// Подписываемся на событие, чтобы обновить страницу после сохранения
+        //editPage.Disappearing += (s, args) =>
+        //{
+        //    LoadCards(); // ? обновляем данные при возвращении
+        //};
+        await Navigation.PushAsync(new Addcards(db));
+        // Переключаем режим редактирования
+        bool editing = !TitleEntry.IsEnabled;
+
+        TitleEntry.IsEnabled = editing;
+        DescriptionEntry.IsEnabled = editing;
+
+        if (editing)
+        {
+            await DisplayAlert("Редактирование", "Теперь вы можете изменить данные и нажать Enter для сохранения.", "ОК");
+        }
     }
+
+    private async void OnEntryCompleted(object sender, EventArgs e)
+    {
+        if (currentCard == null) return;
+
+        currentCard.Title = TitleEntry.Text;
+        currentCard.Description = DescriptionEntry.Text;
+
+        await db.UpdateCard(currentCard);
+
+        TitleEntry.IsEnabled = false;
+        DescriptionEntry.IsEnabled = false;
+
+        await DisplayAlert("Сохранено", "Изменения успешно обновлены!", "ОК");
+    }
+}
 }
